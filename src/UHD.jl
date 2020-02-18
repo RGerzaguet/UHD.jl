4c6e1468eab9a497daa73be362f9d3794862ce57
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2020-02-18 16:20:56 +0100

    Adding metadata function for exportation

--- src/UHD.jl
+++ src/UHD.jl
@@ -44,6 +44,9 @@ export updateCarrierFreq!
 #export getRxBuffer, getRxBuffer!
 export getSingleBuffer
 export getBuffer, getBuffer!, setBuffer
+export populateBuffer!
+export getMetadata
+export getError, getTimestamp
 
 
 # ---------------------------------------------------- 
