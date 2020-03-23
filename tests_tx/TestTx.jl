1831cbc49d85fc50f73a6d7a5d2a1915f57462f7
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2020-03-23 15:33:04 +0100

    Adding testing method for Tx

---
+++ tests_tx/TestTx.jl
@@ -0,0 +1,37 @@
+
+module TestTx
+# ---------------------------------------------------- 
+# --- Modules  
+# ---------------------------------------------------- 
+# --- External modules 
+using FFTW 
+using Printf
+using UHD 
+using Random
+
+function main()	
+	# ---------------------------------------------------- 
+	# --- Physical layer and RF parameters 
+	# ---------------------------------------------------- 
+	carrierFreq		= 770e6;		
+	samplingRate	= 8e6; 
+	txGain			= 50.0; 
+	nbSamples		= 1000;
+	# --- Setting a very first configuration 
+	global radio = setTxRadio("",carrierFreq,samplingRate,txGain); 
+	print(radio);
+	# --- Get samples 
+    buffer	= randn(Complex{Cfloat},radio.packetSize);
+    buffer	= randn(Cfloat,2*radio.packetSize);
+
+	nbEch	= sendBuffer(radio,buffer,true)
+	#for iN = 1 : 1 : 1000
+	#nbEch = sendBuffer(radio,buffer);
+	#@show Int(nbEch);
+	#end
+	@show Int(nbEch);
+	# --- Release USRP 
+	free(radio);
+end
+
+end
