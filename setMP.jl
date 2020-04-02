2bdbdc7019d02acbe4c97c21fda35608d1f7a0fb
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2020-04-02 14:09:27 +0200

    Clen unncessary testing files

--- setMP.jl
+++
@@ -1,9 +0,0 @@
-using Distributed
-addprocs(2; exeflags="--project")
-
-@info "Creating a MP environment based on 2 procs\n";
-@everywhere using Revise;
-@info "Load code everywhere\n";
-@everywhere includet("tests/TestMP.jl");
-@info "MP environment is ready\n";
-
